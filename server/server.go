33d235e200f441f1e9b1a1258bf0373b074bedf4
Dimanda <traperwaze@gmail.com>
2023-03-27 09:26:47 +0700

    serve static from web/.output/public

--- server/server.go
+++ server/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/9d4/netpilot/database"
 	"github.com/9d4/netpilot/ros/board"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	jww "github.com/spf13/jwalterweatherman"
 )
 
@@ -22,8 +23,22 @@ func Start(config *Config) {
 // define routes here
 func applyRoutes(app *fiber.App) {
 	apiRouter := app.Group("/api")
+	apiRouter.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+	}))
 
 	board.NewBoardHandler(database.DB()).SetupRoutes(apiRouter)
+
+	app.Static("/", "./web/.output/public", fiber.Static{
+		Compress: true,
+		Index:    "index.html",
+	})
+	app.Get("/*", func(c *fiber.Ctx) error {
+		return c.SendFile("./web/.output/public/index.html", true)
+	})
 }
 
 // checkError breaks the engine with log.Fatal if err != nil
